09eeed372a150b699db3ef2332a8e8215ae9eb62
nini <nini.tran@outlook.com>
2026-07-11 11:49:43 +0200

    Add break time input to setup dialog

--- src/main/java/SetupDialog.java
+++ src/main/java/SetupDialog.java
@@ -9,13 +9,14 @@ public class SetupDialog {
     private JDialog dialog;
     private JTextField timeField;
     private JTextField roundsField;
+    private JTextField breakTimeField;
     private JButton confirmButton;
     private TimerWindow timerWindow;
 
     public SetupDialog(JFrame parentWindow, TimerWindow timerWindow) {
         this.timerWindow = timerWindow;
         dialog = new JDialog(parentWindow, "Set Time", true);
-        dialog.setSize(400, 300);
+        dialog.setSize(400, 400);
         dialog.setLocationRelativeTo(parentWindow);
         dialog.setLayout(new BorderLayout());
 
@@ -30,26 +31,35 @@ public class SetupDialog {
         roundsField.setFont(new Font("Arial", Font.PLAIN, 30));
         roundsField.setPreferredSize(new Dimension(100, 40));
 
+        breakTimeField = new JTextField();
+        breakTimeField.setFont(new Font("Arial", Font.PLAIN, 30));
+        breakTimeField.setPreferredSize(new Dimension(100, 40));
+
         JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         inputPanel.add(timeField);
 
         JLabel roundsLabel = new JLabel("Rounds");
-        roundsLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        roundsLabel.setFont(new Font("Arial", Font.BOLD, 30));
 
         JPanel roundsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         roundsPanel.add(roundsField);
 
+        JLabel breakTimeLabel = new JLabel("Break for 00:00");
+        breakTimeLabel.setFont(new Font("Arial", Font.BOLD, 30));
+
+        JPanel breakTimePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        breakTimePanel.add(breakTimeField);
 
         timeField.getDocument().addDocumentListener(new DocumentListener() {
 
             private void updatePreview() {
-                String userInput = timeField.getText();
+                String userTimeInput = timeField.getText();
 
-                if (userInput.isEmpty()) {
+                if (userTimeInput.isEmpty()) {
                     previewLabel.setText("00:00");
                     return;
                 }
-                int timeInput = Integer.parseInt(userInput);
+                int timeInput = Integer.parseInt(userTimeInput);
                 int minutes = timeInput / 100;
                 int seconds = timeInput % 100;
 
@@ -73,6 +83,43 @@ public class SetupDialog {
 
         });
 
+        breakTimeField.getDocument().addDocumentListener(new DocumentListener() {
+
+            private void updatePreview() {
+                String userBreakInput = breakTimeField.getText();
+
+                if (userBreakInput.isEmpty()) {
+                    breakTimeLabel.setText("00:00");
+                    return;
+                }
+
+                int timeInput = Integer.parseInt(userBreakInput);
+                int minutes = timeInput / 100;
+                int seconds = timeInput % 100;
+
+                breakTimeLabel.setText(String.format("%02d:%02d", minutes, seconds));
+            }
+
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updatePreview();
+
+
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updatePreview();
+
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updatePreview();
+
+            }
+        });
+
         confirmButton = new JButton("OK");
         confirmButton.setFont(new Font("Arial", Font.BOLD, 24));
 
@@ -83,6 +130,8 @@ public class SetupDialog {
         inputPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         roundsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         roundsPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        breakTimeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        breakTimePanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         confirmButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         centerPanel.add(Box.createVerticalGlue());
@@ -91,83 +140,101 @@ public class SetupDialog {
         centerPanel.add(inputPanel);
         centerPanel.add(roundsLabel);
         centerPanel.add(roundsPanel);
-        centerPanel.add(Box.createVerticalStrut(40));
+        centerPanel.add(breakTimeLabel);
+        centerPanel.add(breakTimePanel);
+        centerPanel.add(Box.createVerticalStrut(20));
         centerPanel.add(confirmButton);
         centerPanel.add(Box.createVerticalGlue());
 
         dialog.add(centerPanel, BorderLayout.CENTER);
 
-        timeField.addActionListener(event -> {
+        timeField.addActionListener(e -> {
             roundsField.requestFocusInWindow();
         });
 
-        roundsField.addActionListener(event -> {
+        roundsField.addActionListener(e -> {
+            breakTimeField.requestFocusInWindow();
+        });
+
+        breakTimeField.addActionListener(e -> {
             confirmButton.doClick();
         });
 
-        confirmButton.addActionListener(event -> {
+        confirmButton.addActionListener(e -> {
 
             try {
-                String userInput = timeField.getText();
+                String userTimeInput = timeField.getText();
                 String roundsInput = roundsField.getText();
-                int timeInput = Integer.parseInt(userInput);
+                String userBreakInput = breakTimeField.getText();
+                int timeInput = Integer.parseInt(userTimeInput);
                 int rounds = Integer.parseInt(roundsInput);
-                int minutes = timeInput / 100;
-                int seconds = timeInput % 100;
-                int totalSeconds = minutes * 60 + seconds;
+                int breakInput = Integer.parseInt(userBreakInput);
+                int totalSeconds = convertMmssToSeconds(timeInput);
+                int totalBreakSeconds = convertMmssToSeconds(breakInput);
 
-                if (totalSeconds <= 0) {
+                if (hasInvalidSeconds(timeInput)) {
                     JOptionPane.showMessageDialog(dialog, "Time must be bigger than 0!");
                     timeField.setText("");
                     timeField.requestFocus();
                     return;
                 }
 
-                if (seconds >= 60) {
-                    JOptionPane.showMessageDialog(dialog, "Last two digits must must be < 60!");
+                    if (rounds <= 0) {
+                        JOptionPane.showMessageDialog(dialog, "Rounds must be bigger than 0");
+                        roundsField.setText("");
+                        roundsField.requestFocus();
+                        return;
+                    }
+
+                    if (hasInvalidSeconds(breakInput)) {
+                        JOptionPane.showMessageDialog(dialog, "Timer must be bigger than 0");
+                        breakTimeField.setText("");
+                        breakTimeField.requestFocus();
+                        return;
+                    }
+
+                    timerWindow.setTimerSettings(totalSeconds, rounds);
+                    dialog.dispose();
+
+                } catch(NumberFormatException exception){
+                    JOptionPane.showMessageDialog(dialog, "Numbers only, valid time input and rounds bigger than 0! Ò.ó");
                     timeField.setText("");
                     timeField.requestFocus();
-                    return;
-
                 }
+            });
 
-                if (rounds <= 0) {
-                    JOptionPane.showMessageDialog(dialog, "Rounds must be bigger than 0");
-                    roundsField.setText("");
-                    roundsField.requestFocus();
-                    return;
+            JRootPane rootPane = dialog.getRootPane();
+            InputMap inputMap = rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+            ActionMap actionMap = rootPane.getActionMap();
+
+            inputMap.put(KeyStroke.getKeyStroke("SPACE"), "pressConfirm");
+            inputMap.put(KeyStroke.getKeyStroke("ESCAPE"), "closeDialog");
+
+            actionMap.put("pressConfirm", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    confirmButton.doClick();
                 }
+            });
+            actionMap.put("closeDialog", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dialog.dispose();
+                }
+            });
 
-                timerWindow.setTimerSettings(totalSeconds, rounds);
-                dialog.dispose();
+            dialog.setVisible(true);
+        }
 
-            } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(dialog, "Numbers only, valid time input and rounds bigger than 0! Ò.ó");
-                timeField.setText("");
-                timeField.requestFocus();
-            }
-        });
+        private int convertMmssToSeconds ( int input) {
+            int minutes = input / 100;
+            int seconds = input % 100;
 
-        JRootPane rootPane = dialog.getRootPane();
-        InputMap inputMap = rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-        ActionMap actionMap = rootPane.getActionMap();
+            return minutes * 60 + seconds;
+        }
 
-        inputMap.put(KeyStroke.getKeyStroke("SPACE"), "pressConfirm");
-        inputMap.put(KeyStroke.getKeyStroke("ESCAPE"), "closeDialog");
-
-        actionMap.put("pressConfirm", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                confirmButton.doClick();
-            }
-        });
-        actionMap.put("closeDialog", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dialog.dispose();
-            }
-        });
-
-        dialog.setVisible(true);
+        private boolean hasInvalidSeconds(int input) {
+            int seconds = input % 100;
+            return seconds >= 60;
+        }
     }
-}
